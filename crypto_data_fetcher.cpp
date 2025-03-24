4b5932064e39b434582a1551f39028032de968cd
exora8 <exorafuturesai@gmail.com>
2025-03-24 23:22:21 +0700

    Update crypto_data_fetcher.cpp

--- crypto_data_fetcher.cpp
+++ crypto_data_fetcher.cpp
@@ -14,8 +14,8 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* out
 }
 
 // Fetch historical crypto price data from API
-std::string fetch_historical_data(const std::string& symbol) {
-    std::string url = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" + symbol + "&tsym=USD&limit=2000";
+std::string fetch_historical_data(const std::string& symbol, const std::string& timeframe) {
+    std::string url = "https://min-api.cryptocompare.com/data/v2/histo" + timeframe + "?fsym=" + symbol + "&tsym=USD&limit=2000";
     CURL* curl = curl_easy_init();
     std::string response;
 
@@ -25,7 +25,7 @@ std::string fetch_historical_data(const std::string& symbol) {
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
         CURLcode res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
-        
+
         if (res != CURLE_OK) {
             std::cerr << "Failed to fetch data: " << curl_easy_strerror(res) << std::endl;
             return "";
@@ -34,24 +34,26 @@ std::string fetch_historical_data(const std::string& symbol) {
     return response;
 }
 
-// Save historical price data to CSV file
-void save_historical_to_csv(const std::string& filename, const std::string& symbol, const std::string& data) {
-    std::ofstream file(filename);
+// Save price data to CSV file
+void save_to_csv(const std::string& filename, const std::string& symbol, const std::string& data) {
+    std::ofstream file(filename, std::ios::app);
     if (file.is_open()) {
-        file << "Date,Symbol,Price(USD)\n";
+        file << "time,price" << std::endl;
         size_t pos = 0;
-        while ((pos = data.find("{")) != std::string::npos) {
-            size_t time_pos = data.find("\"time\":", pos);
-            size_t close_pos = data.find("\"close\":", pos);
-            
-            if (time_pos != std::string::npos && close_pos != std::string::npos) {
-                long timestamp = std::stol(data.substr(time_pos + 7, data.find(',', time_pos) - time_pos - 7));
-                float price = std::stof(data.substr(close_pos + 8, data.find(',', close_pos) - close_pos - 8));
-                
-                std::time_t time = timestamp;
-                file << std::ctime(&time) << "," << symbol << "," << price << "\n";
+        std::string token;
+
+        while ((pos = data.find("},{")) != std::string::npos) {
+            token = data.substr(0, pos);
+            size_t time_pos = token.find("time":);
+            size_t price_pos = token.find("close":);
+
+            if (time_pos != std::string::npos && price_pos != std::string::npos) {
+                std::string timestamp = token.substr(time_pos + 6, 10);
+                std::string price = token.substr(price_pos + 7, token.find(',', price_pos) - price_pos - 7);
+
+                file << timestamp << "," << price << std::endl;
             }
-            data.erase(0, pos + 1);
+            data.erase(0, pos + 2);
         }
         file.close();
     } else {
@@ -60,17 +62,21 @@ void save_historical_to_csv(const std::string& filename, const std::string& symb
 }
 
 int main() {
-    std::string symbol;
+    std::string symbol, timeframe;
     std::cout << "Enter cryptocurrency symbol (e.g., BTC): ";
     std::cin >> symbol;
 
-    std::string filename = symbol + "_historical_data.csv";
-    std::cout << "Fetching full historical data for " << symbol << "..." << std::endl;
+    std::cout << "Enter timeframe (day/hour/minute): ";
+    std::cin >> timeframe;
 
-    std::string data = fetch_historical_data(symbol);
+    std::string filename = symbol + "_historical_data.csv";
+
+    std::cout << "Fetching historical data for " << symbol << " with " << timeframe << " timeframe..." << std::endl;
+    
+    std::string data = fetch_historical_data(symbol, timeframe);
     if (!data.empty()) {
-        save_historical_to_csv(filename, symbol, data);
-        std::cout << "Data saved to " << filename << "\n";
+        save_to_csv(filename, symbol, data);
+        std::cout << "Data saved to " << filename << std::endl;
     }
 
     return 0;
